1690bfaef18f80e8dd5c2d338f0352333c8aba6f
Johnny Dalen <johnny@jumpingmushroom.com>
2026-04-13 11:29:22 +0200

    Add diagnostics tests

---
+++ tests/test_diagnostics.py
@@ -0,0 +1,20 @@
+"""Tests for Nordic Parcel diagnostics."""
+
+from __future__ import annotations
+
+from custom_components.nordic_parcel.diagnostics import _mask_tracking_id
+
+
+class TestMaskTrackingId:
+
+    def test_masks_long_id(self):
+        assert _mask_tracking_id("370000000000123456") == "**************3456"
+
+    def test_short_id_unchanged(self):
+        assert _mask_tracking_id("AB") == "AB"
+
+    def test_exactly_four_chars(self):
+        assert _mask_tracking_id("ABCD") == "ABCD"
+
+    def test_five_chars(self):
+        assert _mask_tracking_id("ABCDE") == "*BCDE"
